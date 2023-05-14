0020e411c752bfc24066f403fb80625151de7797
Anastacia210 <anastacia210@gmail.com>
2023-05-14 14:30:42 +0300

    Ура!!! я все виправила!)))

--- Lesson3(JS)/les3_additional2_JS.js
+++ Lesson3(JS)/les3_additional2_JS.js
@@ -230,18 +230,18 @@ let usersList = [
         }
     }
 ];
-document.write(`<div class="users-box>"`);
+
+document.write(`<div class="users-box">`);
 for (const user of usersList) {
     document.write(`<div class="user-block">
-                       <h2>${user.id} - ${user.name} - ${user.username}</h2>
-                       <h3>${user.email} - ${user.phone}</h3>
-                       <div class="address-block">
-                             <p>City - ${user.address.city}</p> 
-                             <p>Street - ${user.address.street}</p> 
-                             <p>Suite - ${user.address.suite}</p> 
-                             <p>Zip code - ${user.address.zipcode}</p> 
-                   </div>`);
-    document.write(`</div>`);
-
-
+    <h2>${user.id} - ${user.name} - ${user.username}</h2>
+    <h3>${user.email} - ${user.phone}</h3>
+    <div class="address-block">
+        <p>City - ${user.address.city}</p>
+        <p>Street - ${user.address.street}</p>
+        <p>Suite - ${user.address.suite}</p>
+        <p>Zip code - ${user.address.zipcode}</p>
+    </div>
+    </div>`);
 }
+document.write(`</div>`);
